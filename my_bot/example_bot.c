f9634b3ace4da4d1e83c4cb7828692fa2199aca1
Robotino04 <100292446+Robotino04@users.noreply.github.com>
2025-09-26 10:17:07 +0200

    Minimize to 999

--- my_bot/example_bot.c
+++ my_bot/example_bot.c
@@ -1,378 +1,233 @@
 #include "chessapi.h"
-#include <string.h>
-#include <stdint.h>
-#include <stdlib.h>
-#include <math.h>
-#include <stdio.h>
-#include <limits.h>
-#include <assert.h>
-#include <stdbit.h>
+#include "stdlib.h"
+#include "math.h"
+#include "stdbit.h"
 
-// remove static
-static Board* board;
-static uint64_t time_left;
+Board* board;
+int depthleft, time_left;
+GameState state;
+float score;
 
-#define TRANSPOSITION_SIZE (1 << 25)
+#define if \
+    ;if(
+#define bclose ){
+#define cret ) return
+#define bsem );
+#define oboard ( board
+#define semclose \
+    ;            \
+    }
+#define bcom ) ,
 
-// TODO: inline everywhere
-enum {
-    TYPE_EXACT,
-    TYPE_UPPER_BOUND,
-    TYPE_LOWER_BOUND
-};
+#define break \
+    ;         \
+    break semclose
 
-typedef struct {
-    uint64_t hash;
-    float eval;
-    int depth;
-    int type;
-} TranspositionEntry;
-TranspositionEntry* transposition_table;
-// TODO: remove stats
-uint64_t hashes_used = 0;
+#define FOR_MOVES \
+    ;for (int i = 0; i < len_moves; i++ bclose
+#define max_score fmaxf(score,
+#define FREE_MOVES chess_free_moves_array(moves bsem
+#define MAKE_MOVE chess_make_move oboard, moves[i] bsem
+#define UNDO_MOVE chess_undo_move oboard bsem
+#define DEREF_ENTRY entry->
+#define IF_STATE_EQUALS \
+    if state            \
+    ==
+#define GET_SORTED_MOVES \
+    int len_moves; auto moves = chess_get_legal_moves oboard, &len_moves bsem qsort(moves, len_moves, sizeof(Move bcom  compareMoves bsem
 
-
-// TODO
-// - [ ] macro away the board parameter: #define chess_get_legal_moves(...) chess_get_legal_moves(board, __VA_ARGS__)
-// - [ ] remove braces of single-line if-statements
-// - [ ] make common parameters globals
-// - [ ] typedef unsigned long
-// - [ ] make `nodes` global
-// - [ ] test without custom libchess build
-// - [ ] remove all const
-// - [ ] make all macros single line
-// - [ ] remove stats and printing
-
-/*
-int countBit1Fast(unsigned long n) {
-    int c = 0;
-    for (; n; ++c)
-        n &= n - 1;
-    return c;
-}
-*/
+#define BV_EQUALS bestValue =
+#define SCORE_EQUALS score =
+#define ALPHA_BETA BV_EQUALS max_score bestValue bsem alpha = max_score  alpha) if score >= beta) break
+#define IF_TIME_UP                      \
+    if chess_get_elapsed_time_millis () \
+    > time_left
 
 
-// notshit fen: r3k2r/p1p2ppp/2pp4/4p3/P7/2P1PbP1/RP5P/2Q2K1R w kq - 0 20
-// r1bqk1nr/pppp2pp/2n2p2/6B1/1b5Q/P7/1PP1PPPP/RN2KBNR w KQkq - 3 6
-// rn2k1nr/ppp2ppp/4p3/2bp1b2/7q/P7/RPPPPPPP/1NBQKBNR w Kkq - 6 7
-//
-// endgame: 8/8/4k3/8/8/4K3/4R3/4R3 w - - 0 1
-// endgame nodraw: k7/8/1R6/1K6/8/8/8/8 w - - 18 10
-// 2k5/8/2K5/8/8/8/4R3/1R6 w - - 16 9
-// 3k4/8/8/8/8/8/8/3R3K b - - 0 1
-// 8/4k3/8/4K3/8/8/8/2R5 w - - 41 22
-//
-// midgame fail: r5k1/p6p/6p1/2Qb1r2/P6K/8/RP5P/6R1 w - - 0 33
-// prevent promotion: 8/3K4/4P3/8/8/8/6k1/7q w - - 0 1
+struct TranspositionEntry {
+    int64_t hash, depth, type;
+    float eval semclose transposition_table[0b10000000000000000000000000];
 
-float material_of(PlayerColor color) {
-    return (float)stdc_count_ones_ul(chess_get_bitboard(board, color, PAWN)) * 100.0f
-         + (float)stdc_count_ones_ul(chess_get_bitboard(board, color, KNIGHT)) * 300.0f
-         + (float)stdc_count_ones_ul(chess_get_bitboard(board, color, BISHOP)) * 320.0f
-         + (float)stdc_count_ones_ul(chess_get_bitboard(board, color, ROOK)) * 500.0f
-         + (float)stdc_count_ones_ul(chess_get_bitboard(board, color, QUEEN)) * 900.0f;
-}
+#define COUNT_COL(TYPE) +stdc_count_ones_ul(chess_get_bitboard(board, color, TYPE))*
+
+float material_of(PlayerColor color bclose
+    return 0.0f COUNT_COL(PAWN) 100.0f COUNT_COL(KNIGHT) 300.0f COUNT_COL(BISHOP) 320.0f COUNT_COL(ROOK
+    ) 500.0f COUNT_COL(QUEEN) 900.0f semclose
+
+#define chess_get_bitboard_color chess_get_bitboard oboard, color
 
 #define GET_ENDGAME_WEIGHT(COLOR)                                                          \
-    chess_get_bitboard(board, COLOR, KNIGHT) | chess_get_bitboard(board, COLOR, BISHOP)    \
-        | chess_get_bitboard(board, COLOR, ROOK) | chess_get_bitboard(board, COLOR, QUEEN) \
-        | chess_get_bitboard(board, COLOR, KING)
+    chess_get_bitboard_color  COLOR, KNIGHT) | chess_get_bitboard_color  COLOR, BISHOP)    \
+        | chess_get_bitboard_color  COLOR, ROOK) | chess_get_bitboard_color  COLOR, QUEEN) \
+        | chess_get_bitboard_color  COLOR, KING)
 
-float static_eval_me(PlayerColor color) {
-    // TODO: remove before submission
-    static_assert(WHITE == 0, "WHITE isn't 0");
-    static_assert(BLACK == 1, "BLACK isn't 1");
-    static_assert((WHITE ^ 1) == BLACK, "WHITE isn't inverse of BLACK");
-    static_assert((BLACK ^ 1) == WHITE, "BLACK isn't inverse of WHITE");
+float static_eval_me(PlayerColor color bclose
+    float material = material_of(color bcom  material2 = material_of(color ^ 1),
+          endgame_weight = 1.0f - stdc_count_ones_ul(GET_ENDGAME_WEIGHT() | GET_ENDGAME_WEIGHT(^1)) / 16.0f;
 
-    float material = material_of(color);
-    const float material2 = material_of(color ^ 1);
+    int king = chess_get_index_from_bitboard(chess_get_bitboard_color  , KING) bcom 
+        king2 = chess_get_index_from_bitboard(chess_get_bitboard_color  ^ 1, KING))
 
-    float endgame_weight = (1.0f - (((float)stdc_count_ones_ul(GET_ENDGAME_WEIGHT(WHITE) | GET_ENDGAME_WEIGHT(BLACK))) / 16.0f));
-
-    int king = chess_get_index_from_bitboard(chess_get_bitboard(board, color, KING));
-    int king2 = chess_get_index_from_bitboard(chess_get_bitboard(board, color ^ 1, KING));
-
-    if (material > material2 + 200) {
-        int file = king2 % 8;
-        int rank = king2 / 8;
-        int dist_to_edge = fmin(file, 7 - file) + fmin(rank, 7 - rank);
+    if material > material2 + 200 bclose
+        int file = king2 % 8, rank = king2 / 8, dist_to_edge = fminf(file, 7 - file) + fminf(rank, 7 - rank bsem
         material += (7 - dist_to_edge) * endgame_weight * 5.0f;
+        material += (14 - abs(king % 8 - file) - abs(king / 8 - rank)) * endgame_weight * 1.0f semclose 
 
-        material += (14 - (abs(king % 8 - king2 % 8) + abs(king / 8 - king2 / 8))) * endgame_weight * 1.0f;
-    }
+return material semclose 
 
-    return material;
+
+float static_eval( bclose
+    IF_STATE_EQUALS GAME_CHECKMATE cret -INFINITY
+    IF_STATE_EQUALS GAME_STALEMATE cret 0;
+
+
+    return (chess_is_white_turn oboard) ? 1.0f : -1.0f) * (static_eval_me(WHITE) - static_eval_me(BLACK) bsem
 }
 
-float static_eval(GameState state) {
-    if (state == GAME_CHECKMATE) {
-        return -INFINITY;
-    }
 
-    if (state == GAME_STALEMATE) {
-        return 0;
-    }
-
-    return (chess_is_white_turn(board) ? 1.0f : -1.0f) * (static_eval_me(WHITE) - static_eval_me(BLACK));
-}
-
-#define GEN_HASH                                                                \
-    uint64_t hash_orig = chess_zobrist_key(board);                              \
-    /* only works for powers of two */                                          \
-    uint64_t hash = (hash_orig ^ (hash_orig >> 32)) & (TRANSPOSITION_SIZE - 1); \
-    TranspositionEntry* entry = &transposition_table[hash];
+#define GEN_HASH                                                                                                              \
+    uint64_t hash_orig = chess_zobrist_key oboard bcom  hash = (hash_orig ^ (hash_orig >> 32)) & 0b01111111111111111111111111 ; \
+    auto entry = &transposition_table[hash];
 
 
-float scoreMove(Move* move) {
-    chess_make_move(board, *move);
+float scoreMove(Move* move bclose
+    chess_make_move oboard, *move bsem
     GEN_HASH
-    chess_undo_move(board);
+    UNDO_MOVE
 
-    PieceType movePiece = chess_get_piece_from_bitboard(board, move->from);
+    PieceType movePiece = chess_get_piece_from_bitboard oboard, move->from bsem
 
-    float score = 0.0f;
-    if (move->capture) {
-        score = 10.0f * chess_get_piece_from_bitboard(board, move->to) - movePiece;
+    SCORE_EQUALS 0.0f
+    if move->capture)
+        SCORE_EQUALS 10.0f * chess_get_piece_from_bitboard oboard, move->to) - movePiece;
+
+
+    return score + move->promotion + fmaxf( DEREF_ENTRY depth - 1, 0) * 100 + (entry->depth >= 2 ? DEREF_ENTRY eval : 0 bsem
     }
 
-    if (move->promotion) { // "if" can be omitted
-        score += move->promotion;
-    }
+int compareMoves(const void* a, const void* b bclose
+    float sa = scoreMove(a bcom  sb = scoreMove(b)
 
-    // probably possible with only checking once
-    score += fmax(entry->depth - 1, 0) * 100 + (entry->depth >= 2 ? entry->eval : 0);
+    if sa < sb cret 1
+    if sa > sb cret -1;
+    return 0 semclose 
+
+float quiescence(float alpha, float beta bclose
+    IF_TIME_UP cret 0;
 
 
-    /* maybe if we get the bitboards
-    int movelen;
-    chess_skip_turn(board);
-    Move* moves = chess_get_legal_moves(board, &movelen);
+    state = chess_get_game_state oboard)
+    IF_STATE_EQUALS GAME_STALEMATE cret 0;
 
-    BitBoard all_opp_attacked = 0;
-    for (int i = 0; i < movelen; i++) {
-        all_opp_attacked |= moves[i].to;
-    }
+    float BV_EQUALS SCORE_EQUALS static_eval( bcom  score
+    if bestValue >= beta cret bestValue;
 
-    chess_free_moves_array(moves);
-    chess_undo_move(board);
+    alpha = max_score alpha bsem
 
-    if (move->to & all_opp_attacked) {
-        score -= movePiece;
-    }
-    */
+    GET_SORTED_MOVES
+    bool is_check = chess_in_check oboard)
+    if len_moves == 0 && !is_check)
+        BV_EQUALS 0
 
-    return score;
+
+    FOR_MOVES
+        if moves[i].capture || is_check bclose
+            MAKE_MOVE
+            SCORE_EQUALS -quiescence(-beta, -alpha bsem
+            UNDO_MOVE
+
+            IF_TIME_UP bclose
+                BV_EQUALS 54321.f
+                break 
+
+
+                ALPHA_BETA
 }
 
-int compareMoves(const void* a, const void* b) {
-    float sa = scoreMove((Move*)a); // TODO: remove cast
-    float sb = scoreMove((Move*)b); // TODO: remove cast
-
-    if (sa < sb)
-        return 1;
-    if (sa > sb)
-        return -1;
-    return 0;
-}
-
-void orderMoves(Move* moves, int len) {
-    qsort(moves, len, sizeof(Move), compareMoves);
-}
+    return bestValue semclose 
 
 
-float quiescence(float alpha, float beta, long* nodes) {
-    if (chess_get_elapsed_time_millis() > time_left) {
-        return 54321.0f;
-    }
-    ++*nodes;
+float alphaBeta(float alpha, float beta bclose
+    IF_TIME_UP cret 0
 
-    GameState state = chess_get_game_state(board);
-    if (state == GAME_STALEMATE) {
-        return 0;
-    }
-    float bestValue = static_eval(state);
-    if (bestValue >= beta) {
-        return bestValue;
-    }
-    alpha = fmaxf(alpha, bestValue);
+    if depthleft <= 0 cret quiescence(alpha, beta bsem
 
-    int len_moves;
-    Move* moves = chess_get_legal_moves(board, &len_moves);
-    orderMoves(moves, len_moves);
-    bool is_check = chess_in_check(board);
-    if (len_moves == 0 && !is_check) { // can probably be thrown out for tokens later
-        bestValue = 0;
-    }
-
-    for (int i = 0; i < len_moves; i++) {
-        // maybe invert and wrap everything
-        if (!(moves[i].capture || is_check)) {
-            continue;
-        }
-
-        chess_make_move(board, moves[i]);
-        float score = -quiescence(-beta, -alpha, nodes);
-        chess_undo_move(board);
-
-        if (chess_get_elapsed_time_millis() > time_left) {
-            bestValue = 54321.f;
-            break;
-        }
-
-        bestValue = fmaxf(score, bestValue);
-        alpha = fmaxf(score, alpha);
-        if (score >= beta) {
-            break;
-        }
-    }
-done:
-
-    chess_free_moves_array(moves);
-    return bestValue;
-}
-
-float alphaBeta(float alpha, float beta, int depthleft, long* nodes) {
-    if (chess_get_elapsed_time_millis() > time_left) {
-        return -12345.f;
-    }
-
-    if (depthleft <= 0) {
-        return quiescence(alpha, beta, nodes);
-    }
     float alpha_orig = alpha;
 
-    // TODO: remove assert
-    static_assert(
-        (TRANSPOSITION_SIZE & (TRANSPOSITION_SIZE - 1)) == 0,
-        "TRANSPOSITION_SIZE isn't a power of two"
-    );
     GEN_HASH
-    if (entry->depth >= depthleft && entry->hash == hash_orig) {
-        // TODO: move into a single if
-        if (entry->type == TYPE_EXACT) {
-            return entry->eval;
-        }
-        if (entry->type == TYPE_LOWER_BOUND && entry->eval >= beta) {
-            return entry->eval;
-        }
-        if (entry->type == TYPE_UPPER_BOUND && entry->eval < alpha) {
-            return entry->eval;
-        }
-    }
 
-    // quiescence will also instantly return 0 for draws
-    // this saves a bit of performance
-    // TODO: inline
-    GameState state = chess_get_game_state(board);
-    if (state == GAME_STALEMATE) {
-        return 0;
-    }
+    if  DEREF_ENTRY depth >= depthleft && entry->hash == hash_orig && !entry->type || entry->type == 2
+        &&  DEREF_ENTRY eval >= beta || entry->type == 1 && DEREF_ENTRY eval < alpha
+        cret  DEREF_ENTRY eval;
 
-    ++*nodes;
+    state = chess_get_game_state oboard)
 
-    float bestValue = -INFINITY;
-    int len_moves;
-    Move* moves = chess_get_legal_moves(board, &len_moves);
-    orderMoves(moves, len_moves);
+    IF_STATE_EQUALS GAME_STALEMATE cret 0;
 
-    for (int i = 0; i < len_moves; i++) {
-        chess_make_move(board, moves[i]);
-        float score = -alphaBeta(-beta, -alpha, depthleft - 1, nodes);
-        chess_undo_move(board);
 
-        if (chess_get_elapsed_time_millis() > time_left) {
-            bestValue = 12345.f;
-            break;
-        }
+    float BV_EQUALS -INFINITY, score;
+    GET_SORTED_MOVES 
 
-        bestValue = fmaxf(score, bestValue);
-        alpha = fmaxf(score, alpha);
-        if (score >= beta) {
-            break;
-        }
-    }
-done:
+    FOR_MOVES
+        MAKE_MOVE
+        depthleft--;
+        SCORE_EQUALS -alphaBeta(-beta, -alpha bsem
+        depthleft++;
+        UNDO_MOVE
 
-    chess_free_moves_array(moves);
+        IF_TIME_UP bclose
+            BV_EQUALS 12345.f
+            break 
 
-    // TODO: maybe redundant, but lets leave it here for now
-    if (entry->depth <= depthleft) {
-        // TODO: stats
-        if (entry->depth == -1) {
-            hashes_used++;
-        }
+        ALPHA_BETA
 
-        entry->hash = hash_orig;
-        entry->eval = bestValue;
-        entry->depth = depthleft;
-        entry->type = bestValue <= alpha_orig ? TYPE_UPPER_BOUND
-                    : bestValue >= beta       ? TYPE_LOWER_BOUND
-                                              : TYPE_EXACT;
-    }
+    FREE_MOVES
 
-    return bestValue;
+    if  DEREF_ENTRY depth <= depthleft bclose
+         DEREF_ENTRY hash = hash_orig;
+         DEREF_ENTRY eval = bestValue;
+         DEREF_ENTRY depth = depthleft;
+         DEREF_ENTRY type = bestValue <= alpha_orig ? 1 : bestValue >= beta ? 2 : 0 semclose 
+
+return bestValue semclose 
+
+int main(void bclose
+    while (true bclose
+        board = chess_get_board( bsem
+
+        GET_SORTED_MOVES
+        Move prevBestMove = moves[0], bestMove = prevBestMove; // TODO: move `moves` ptr instead of separate variable
+
+        time_left = chess_get_time_millis( bsem // + increment /2 if we had that
+
+        for (depthleft = 1; depthleft < 100; depthleft++ bclose
+            float BV_EQUALS -INFINITY
+             FOR_MOVES
+                MAKE_MOVE
+                SCORE_EQUALS -alphaBeta(-INFINITY, INFINITY bsem
+                UNDO_MOVE
+
+                IF_TIME_UP)
+                    goto search_canceled
+
+
+                if score > bestValue bclose
+                    BV_EQUALS score;
+                    bestMove = moves[i] semclose
 }
-
-
-int main(int argc, char* argv[]) {
-    // TODO: inline assignment into memset
-    transposition_table = malloc(sizeof(TranspositionEntry) * TRANSPOSITION_SIZE);
-    memset(transposition_table, 0xFF, sizeof(TranspositionEntry) * TRANSPOSITION_SIZE);
-    while (true) {
-        board = chess_get_board();
-
-        int len_moves;
-        Move* moves = chess_get_legal_moves(board, &len_moves);
-        Move prevBestMove = moves[0]; // TODO: move `moves` ptr instead of separate variable
-        Move bestMove = moves[0];
-
-        // TODO: divide by 20 to allow full-game time management
-        time_left = chess_get_time_millis(); // + increment /2 if we had that
-
-        for (int depth = 1; depth < 100; depth++) {
-            float bestValue = -INFINITY;
-            long nodes = 0;
-            for (int i = 0; i < len_moves; i++) {
-                chess_make_move(board, moves[i]);
-                float score = -alphaBeta(-INFINITY, INFINITY, depth, &nodes);
-                chess_undo_move(board);
-
-                if (chess_get_elapsed_time_millis() > time_left) {
-                    goto search_canceled;
-                }
-
-                if (score > bestValue) {
-                    bestValue = score;
-                    bestMove = moves[i];
-                }
-            }
-            if (chess_get_elapsed_time_millis() > time_left) {
+    IF_TIME_UP)
                 goto search_canceled;
-            }
-            printf(
-                "info depth %d score cp %d nodes %lu nps %lu hashfull %lu time %lu\n",
-                depth,
-                (int)bestValue,
-                nodes,
-                (nodes * 1000) / (chess_get_elapsed_time_millis() + 1),
-                hashes_used * 1000 / TRANSPOSITION_SIZE,
-                chess_get_elapsed_time_millis()
-            );
-            fflush(stdout);
-            prevBestMove = bestMove;
-            if (bestValue == INFINITY) {
-                break;
-            }
-        }
+
+prevBestMove = bestMove
+if bestValue == INFINITY)
+                break 
     search_canceled:
 
-        chess_push(prevBestMove);
+        chess_push(prevBestMove bsem
 
-        chess_free_moves_array(moves);
-        chess_free_board(board);
+        FREE_MOVES
+        chess_free_board oboard bsem
 
-        chess_done();
-    }
+        chess_done( bsem
+}
 }
