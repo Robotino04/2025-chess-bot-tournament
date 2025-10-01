9699015eb368d0f8fa4f585ffed6d4880d5ec667
Robotino04 <100292446+Robotino04@users.noreply.github.com>
2025-10-01 17:19:30 +0200

    Add cutechess-cli test script

---
+++ my_bot/test.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+cutechess-cli \
+    -engine "conf=Stockfish 1320" \
+    -engine "conf=Stockfish 1500" \
+    -engine "conf=NB Latest" \
+    -engine "conf=NB v4_transpos" \
+    -engine "conf=NB v5_good_transpos" \
+    -engine "conf=NB v6_transpos_order" \
+    -engine "conf=NB v7_minimized_986" \
+    -each st=0.05 timemargin=100 \
+    -games 250 -concurrency 32 \
+    -ratinginterval 1
