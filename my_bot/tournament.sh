a085f8f3ad32f8d90e25e8376ece4a6fba6877ed
Robotino04 <100292446+Robotino04@users.noreply.github.com>
2025-11-03 22:06:51 +0100

    No longer restart between games

--- my_bot/tournament.sh
+++ my_bot/tournament.sh
@@ -4,12 +4,11 @@ cutechess-cli \
     -engine "conf=Stockfish 1500" \
     -engine "name=NB Latest" cmd=./example_bot \
     -engine "name=NB Latest Minimized" cmd=./example_bot_minimized \
-    -engine "name=NB v4_transpos" cmd=./backups/v4_transpos \
-    -engine "name=NB v5_good_transpos" cmd=./backups/v5_good_transpos \
-    -engine "name=NB v8_really_small" cmd=./backups/v8_really_small \
-    -engine "name=NB v9_negascout" cmd=./backups/v9_negascout \
+    -engine "name=NB v12_root_ordering" cmd=./backups/v12_root_ordering \
+    -engine "name=NB v9_negascout_tc" cmd=./backups/v9_negascout_tc \
     -openings file="book-ply6-unifen_Q.txt" format=epd order=random plies=6 policy=default \
-    -games 250 -concurrency 32 \
+    -games 350 -concurrency 32 \
+    -recover \
     -each tc=1 timemargin=200 proto=uci restart=on \
     -ratinginterval 10
 
