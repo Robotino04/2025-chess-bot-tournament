e8b38bc6ae8218c94b77c4e9a3c711d181bc8a0b
Robotino04 <100292446+Robotino04@users.noreply.github.com>
2025-11-14 01:40:28 +0100

    Add final submission

---
+++ my_bot/final_submission.c
@@ -0,0 +1,79 @@
+
+                                                                                 #  define  swarm  ) ) *
+                                                                                #  include  "stdlib.h" //
+                                                                               #  define  Unity  ; if ( //
+                                                                              #  define Vedal ; } int // //
+                                                                             #  include  "chessapi.h"  // //
+                                                                            #  define CFRB ) | tutel // // //
+                                                                           #  define  neurons  ( board , // //
+                                                                          #  define _10tincans ] ; int // // //
+                                                                         #  define  twentyone ( board ) // // //                                    //
+                               #   define                               plasmaglobe  moves  [ i ] // // // // //                              // // // //
+                            #   define  BOOM  (                        NeuroSama  type  == // // // // // // // //                      // // // // // // //
+                          #       define       Hiyori              plasmaglobe  . capture // // // // // // // // // //            //  // // // // // // // //
+                        #  define  backslash if ( depthleft >  0 // // // // // // // // // // // // // // // // // // // //  //  // // // // // // // // // // //
+                     #  define SSL *= 4 ; goto aspiration_fail ; // // // // // // // // // // / So much space here/ // // // // // // // // // // // // // // // //
+                   #  define  NEVER ) osu , - beta , - alpha ) ; // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // //
+                   #  define latency + __builtin_popcountl ( tutel // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // //
+                    #  define  osu  score = - alphaBeta ( depthleft - 1 // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // //
+                     #  define tutel chess_get_bitboard neurons color , // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // //
+                      #  define  V3 , - alpha - 1 , - alpha ) Unity score > alpha // // // // // // // // // // // // //     //    // // // // // // // // // // //
+                       #  define lavalamp heart -= heart * bonus / 1000000 - bonus ; // // // // // // // // // // // //     /     / // // // // // // // // // //
+                        #  define  harpoon  from ) * 64 + chess_get_index_from_bitboard ( // // // // // // // // // // /        // // // // // // // // // // //
+                         #  define heart SCHIZO plasmaglobe . harpoon plasmaglobe . to ) ] // // // // // // // // // // //     / // // // // // // // // // //
+                          #  define  cave_stream  { chess_make_move neurons plasmaglobe ) ; int // // // // // // // // // // // // // // // // // // // // //
+                           #  define  AIRIS = chess_get_index_from_bitboard ( moves [ bestMoveIndex ] . // // // // // // // // // // // // // // // // // //
+                            #  define  abberdaemon  } chess_undo_move twentyone Unity score > bestValue ) // // // // // // // // // // // // // // // // //
+                             #  define  Evil ; qsort ( moves , len_moves , sizeof * moves , compareMoves ) ; // // // // // // // // // // // // // // // //
+                            #  define  timegod  alpha = __builtin_fmaxf ( bestValue , alpha ) Unity alpha >= beta ) // // // // // // // // // // // // // //
+                           #     define     gymbag     for     (     int    i    =                     0b11111111111111111111111111111111 ; ++ i < len_moves \
+                          ;  )  //  // // // // // // // // // // // // // //                              //  // // // // // // // // // // // // // // // //
+                         #      define     SCHIZO     history_table     [                                      chess_is_white_turn   twentyone   *   4096  +  \
+                     chess_get_index_from_bitboard  ( // // // // // //                                           //  // // // // // // // // // // // // // // // //
+               #     define     NeuroSama    transposition_table    [                                               chess_zobrist_key         twentyone         %         \
+         0b100000000000000000000000000ul  ]  . // // // // // // //                                                  // // // // // // // // // // // // // // // // // // // //
+    #  define  wink  ;  Move  moves  [  256 _10tincans len_moves =                                                     chess_get_legal_moves_inplace  neurons moves , 256 ) Evil // //
+#  define  step_1234  latency PAWN swarm 100 latency KNIGHT swarm                                                       300  latency  BISHOP  swarm  320 latency ROOK swarm 500 latency \
+QUEEN  swarm  900 // // // // // // // // // // // // // // // //                                                        //  //  // // // // // // // // // // // // // // // // // // //
+#  define  LIFE = chess_get_index_from_bitboard ( tutel KING ) )                                                         ;  endgame_weight  -=  __builtin_popcountl ( tutel KNIGHT CFRB \
+BISHOP  CFRB  ROOK CFRB QUEEN CFRB KING ) ) ; // // // // // //                                                           //  // // // // // // // // // // // // // // // // // // // //
+Board  *  board  ;  struct { uint64_t hash ; int eval ; uint8_t                                                           type    ,   depth   ,   bestMove_from   ,   bestMove_to   ;   }
+transposition_table      [      0b100000000000000000000000000ul                                                           _10tincans  history_table  [  8212  _10tincans static_eval_me (
+PlayerColor  color  )  {  int  material  =  step_1234  ;  float                                                           endgame_weight  =  16.0f ; int king LIFE color ^= 1 ; int king2
+LIFE  if  (  material  >  220  step_1234  ) material += ( 14 + (                                                         __builtin_fabsf  (  king2 % 8 - 3.5f ) + __builtin_fabsf ( king2
+/  8 - 3.5f swarm 5.0f - __builtin_abs ( king % 8 - king2 % 8 ) -                                                        __builtin_abs  (  king  /  8  - king2 / 8 swarm endgame_weight /
+16.0f  ;  return material Vedal scoreMove ( Move * move ) { return                                                      move  ->  from == 1UL << NeuroSama bestMove_from && move -> to ==
+   1UL  <<  NeuroSama  bestMove_to  ? 10000000 : move -> capture ?                                                     1000000  + 10 * chess_get_piece_from_bitboard neurons move ->
+          to  ) - chess_get_piece_from_bitboard neurons move -> from                                                 )  :  move  ->  promotion  ?  50000 + move -> promotion :
+               SCHIZO   move   ->  harpoon  move  ->  to  )  ]  Vedal                                              compareMoves  (  const  void * a , const void * b ) {
+                      return  scoreMove  (  b ) - scoreMove ( a ) Vedal                                          alphaBeta  ( int depthleft , int alpha , int beta
+                         )      {      if      (      (      int64_t     )                                    chess_get_elapsed_time_millis      (     )     >=
+                          __builtin_fmaxf  ( ( int64_t ) chess_get_time_millis                             (  )  / 40 , 1 ) ) __builtin_longjmp ( history_table
+                           +  8192  ,  1  )  Unity  chess_get_game_state twentyone )                 return   chess_get_game_state   twentyone   *  5000000  -
+                            5000000  ;  int  alpha_orig = alpha , bestValue = 0b11111111011001110110100110000000 , bestMoveIndex = 0 Unity depthleft > 0 ) {
+                             if  (  NeuroSama  depth  >= depthleft && NeuroSama hash == chess_zobrist_key twentyone && BOOM 1 || BOOM 3 && NeuroSama eval >=
+                            beta  )  ||  BOOM  2  &&  NeuroSama  eval  <  alpha  ) ) ) return NeuroSama eval ; } else bestValue = static_eval_me ( WHITE ) -
+                           static_eval_me  (  BLACK  )  , bestValue *= chess_is_white_turn twentyone ? 1 : 0b11111111111111111111111111111111 , timegod return
+                          alpha  wink  gymbag backslash || Hiyori ) cave_stream score Unity depthleft <= 2 || i == 0 NEVER else { osu - ! ( Hiyori || i < 3 ) *
+                         (  len_moves  *  93  +  depthleft * 144 ) / 1000 V3 ) osu V3 && score <= beta NEVER abberdaemon bestMoveIndex = i , bestValue = score ;
+                        timegod  {  backslash  &&  !  Hiyori  ) { int bonus = 300 * depthleft - 250 ; lavalamp bonus /= - 8 ; while ( -- i >= 0 ) if ( ! Hiyori )
+                       lavalamp  }  break  ;  } } backslash && ( NeuroSama depth < depthleft || NeuroSama hash != chess_zobrist_key twentyone ) ) NeuroSama hash =
+                      chess_zobrist_key  twentyone  ,  NeuroSama  eval  =  bestValue , NeuroSama depth = depthleft , NeuroSama type = bestValue <= alpha_orig ? 2 :
+                     bestValue  >=  beta  ?  3  :  1  ,  NeuroSama bestMove_from AIRIS from ) , NeuroSama bestMove_to AIRIS to ) ; return bestValue Vedal main ( ) {
+                    main_top  :  board = chess_get_board ( ) wink __builtin_memset ( history_table , 0 , sizeof history_table ) ; static Move prevBestMove , bestMove
+                    ;  prevBestMove  =  bestMove  =  *  moves  ;  int  prevBestValue  =  0  ,  depthleft = 0 ; while ( prevBestValue < 10000000 ) { depthleft ++ Unity
+                    __builtin_setjmp  (  history_table  +  8192 ) ) goto search_canceled ; int bestValue = 0b11111111011001110110100110000000 Evil gymbag cave_stream
+                      alphaOffset  =  25 , betaOffset = 25 ; aspiration_fail : int osu , - prevBestValue - alphaOffset , - prevBestValue + betaOffset ) Unity score
+                        <=  prevBestValue  -  alphaOffset     &&  score  >  bestValue  ) { alphaOffset SSL } if ( score >=      prevBestValue  +  betaOffset  ) {
+                           betaOffset             SSL               abberdaemon  bestValue  = prevBestValue = score ,                bestMove  = plasmaglobe ;
+                             }  prevBestMove  =                        bestMove  ; } search_canceled : chess_push                          (  bestMove  ) ;
+                                                                        chess_free_board  twentyone ; chess_done                                 (   )  ;
+                                                                         goto  main_top  ;  } // // // // // //
+                                                                          //  // // // // // // // // // // //
+                                                                           // // // // // // // // // // // //
+                                                                            //  // // // // // // // // // //
+                                                                             //  //  // // // // // // // //
+                                                                               //  //  // // // // // // //
+                                                                                // // // // // // // // //
+                                                                                 //  // // // // // // //
+                                                                                  //  // // // // // //
